c53139a23ce32f1d88f2ae989a02c0ebf39ef193
Kevin Jair Valenzuela Ponce <19170694@itculiacan.edu.mx>
2024-07-17 12:29:26 -0700

    Started to make a Estudiantes table

---
+++ aspnetdemo2/EstudiantesRepository.cs
@@ -0,0 +1,24 @@
+namespace aspnetDemo2{
+    public class EstudiantesRepository{
+    private static List<Estudiante> _estudiantes = new List<Estudiante>();
+        static EstudiantesRepository(){
+            _estudiantes.Add(new Estudiante(){NumeroControl = "123456", Nombre = "Juan Perez", Carrera = "Sistemas"});
+            _estudiantes.Add(new Estudiante(){NumeroControl = "123457", Nombre = "Maria Lopez", Carrera = "Sistemas"});
+        }
+       
+        public List<Estudiante> GetEstudiantes(){
+            
+            return _estudiantes.Select(e=>e).ToList();
+        }
+    } 
+
+    public class Estudiante{
+        public string NumeroControl {get; set;}
+
+        public string Nombre {get; set;}
+
+        public string Carrera {get; set;}
+
+        public float Promedio {get; set;}
+    }
+}
--- aspnetdemo2/Pages/estudiantes/index.cshtml.cs
+++ aspnetdemo2/Pages/estudiantes/index.cshtml.cs
@@ -1,22 +1,40 @@
+using aspnetDemo2;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using System.Collections.Generic; 
 
 namespace aspnetdemo2.Pages.estudiantes;
 
+
 public class IndexModel : PageModel
 {
     public List<EstudiantesEnListaModel> EstudiantesEnLista {get; set;}
     private readonly ILogger<IndexModel> _logger;
+    private EstudiantesRepository repo;
     public IndexModel(ILogger<IndexModel> logger)
     {
         _logger = logger;
+        repo = new EstudiantesRepository();
+        
+
     }
 
     public void OnGet()
     {
-       EstudiantesEnLista = new List<EstudiantesEnListaModel>();
-       EstudiantesEnLista.Add(new EstudiantesEnListaModel(){NumeroControl = "123456", Nombre = "Juan Perez", Carrera = "Sistemas"});
+        var modelo = repo.GetEstudiantes();
+        EstudiantesEnLista = modelo.Select(e => 
+        new EstudiantesEnListaModel()
+        {
+            NumeroControl = e.NumeroControl,
+            Nombre = e.Nombre,
+            Carrera = e.Carrera
+        }
+       
+        ).ToList();
+    //    EstudiantesEnLista = new List<EstudiantesEnListaModel>();
+    //    EstudiantesEnLista.Add(new EstudiantesEnListaModel(){NumeroControl = "123456", Nombre = "Juan Perez", Carrera = "Sistemas"});
+    //    EstudiantesEnLista.Add(new EstudiantesEnListaModel(){NumeroControl = "123457", Nombre = "Maria Lopez", Carrera = "Sistemas"});
+    
     }
 
 
